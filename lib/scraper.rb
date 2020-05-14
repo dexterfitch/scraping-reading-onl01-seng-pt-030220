dde2c598aab662c227335589570fd2cda74e5a89
Dexter Fitch <fitch.dexter@gmail.com>
2020-05-14 18:55:56 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,14 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+puts doc.css(".headline-26OIBN").text
+
+courses = doc.css(".gridContainer-EnvccM .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC .title-oE5vT4")
+
+courses.each do |course|
+  puts course.text.strip
+end
+
+p doc.css(".gridContainer-EnvccM .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0].children
