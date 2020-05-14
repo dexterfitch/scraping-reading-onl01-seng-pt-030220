08b81ba15924fd1927910d0d97bee31d932f7e2a
Dexter Fitch <fitch.dexter@gmail.com>
2020-05-14 18:50:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ doc = Nokogiri::HTML(html)
 puts doc.css(".headline-26OIBN").text
 
 courses = doc.css(".gridContainer-EnvccM .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC .title-oE5vT4")
-
+puts courses
 courses.each do |course|
   puts course.text.strip
 end
