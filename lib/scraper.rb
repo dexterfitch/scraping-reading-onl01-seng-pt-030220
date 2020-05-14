85b3ebe538a42db755a65c54ab8b1b68d2b86440
Dexter Fitch <fitch.dexter@gmail.com>
2020-05-14 18:52:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,4 +12,4 @@ courses.each do |course|
   puts course.text.strip
 end
 
-p doc.css(".gridContainer-EnvccM .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0]
+puts doc.css(".gridContainer-EnvccM .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0]
