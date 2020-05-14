979124ecab18c323acb86721aa966eddbe5efe31
Dexter Fitch <fitch.dexter@gmail.com>
2020-05-14 18:48:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,3 +5,9 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
 puts doc.css(".headline-26OIBN").text
+
+courses = doc.css(".gridContainer-EnvccM .title-oE5vT4")
+
+courses.each do |course|
+  puts course.text
+end
