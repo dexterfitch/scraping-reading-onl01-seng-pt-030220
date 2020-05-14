fa7fd2241653fcbd2919834836abd1540b8cdb24
Dexter Fitch <fitch.dexter@gmail.com>
2020-05-14 18:49:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,5 +9,5 @@ puts doc.css(".headline-26OIBN").text
 courses = doc.css(".gridContainer-EnvccM .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC .title-oE5vT4")
 
 courses.each do |course|
-  puts course.text
+  puts course.text.strip
 end
